bda4159b7a0df206c3fc507d19df97167e88a2ba
vadfq <138349650+vadfq@users.noreply.github.com>
2023-07-02 13:43:52 +0900

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,43 @@
-fa
+import numpy as np
+
+def ssh(n,v,w):     # Hamiltonian matrix of The Su-Schrieffer–Heeger (SSH) model n:전체 격자 수(짝수) v:intracell hopping w:intercell hopping
+
+    lm=[]
+
+    l1=[]
+    l1.append(0)
+    l1.append(v)
+    for i in range(n-2):
+        l1.append(0)
+
+    lm.append(l1)
+
+    l1=[]
+
+    for i in range(n-2):
+        for j in range(n):
+            if i%2==0:
+                if j==i:
+                    l1.append(v)
+                elif j==i+2:
+                    l1.append(w)
+                else:
+                    l1.append(0)
+            else:
+                if j==i:
+                    l1.append(w)
+                elif j==i+2:
+                    l1.append(v)
+                else:
+                    l1.append(0)
+        lm.append(l1)
+        l1=[]
+
+    for i in range(n-2):
+        l1.append(0)
+    l1.append(v)
+    l1.append(0)
+
+    lm.append(l1)
+
+    return(lm)
