20188497a71fa4bec047aa23f552df8662ee03ce
vadfq <138349650+vadfq@users.noreply.github.com>
2023-07-02 14:25:16 +0900

    Update a.py

--- a.py
+++ a.py
@@ -1,43 +1,20 @@
 import numpy as np
 
-def ssh(n,v,w):     # Hamiltonian matrix of The Su-Schrieffer–Heeger (SSH) model n:전체 격자 수(짝수) v:intracell hopping w:intercell hopping
+def ssh(n,v,w):     # The matrix for the Hamiltonian of the Su-Schrieffer–Heeger (SSH) model n:the number of unit cells (even number) v:intracell hopping w:intercell hopping
 
-    lm=[]
+    lm=[[0]*n for _ in range(n)]
+    
+    lm[0][1]=v
 
-    l1=[]
-    l1.append(0)
-    l1.append(v)
-    for i in range(n-2):
-        l1.append(0)
-
-    lm.append(l1)
-
-    l1=[]
-
-    for i in range(n-2):
-        for j in range(n):
-            if i%2==0:
-                if j==i:
-                    l1.append(v)
-                elif j==i+2:
-                    l1.append(w)
-                else:
-                    l1.append(0)
-            else:
-                if j==i:
-                    l1.append(w)
-                elif j==i+2:
-                    l1.append(v)
-                else:
-                    l1.append(0)
-        lm.append(l1)
-        l1=[]
-
-    for i in range(n-2):
-        l1.append(0)
-    l1.append(v)
-    l1.append(0)
-
-    lm.append(l1)
+    for i in range(1,n-1):
+        if i%2==1:
+            lm[i][i-1]=v
+            lm[i][i+1]=w
+                
+        else:
+            lm[i][i-1]=w
+            lm[i][i+1]=v
+        
+    lm[n-1][n-2]=v
 
     return(lm)
